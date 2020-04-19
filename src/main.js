9a2b2c3fd880a9f6af4eea51e3a9174960a53085
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-19 16:37:46 +0530

    Integrated vuex and separted into modules

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import router from "./router";
+import store from "./store";
 
 import AppDropdown from "./components/shared/AppDropdown";
 import AppHero from "./components/shared/AppHero";
+import AppSpinner from "./components/shared/AppSpinner.vue";
 
 import moment from "moment";
 
@@ -13,6 +15,7 @@ Vue.config.productionTip = false;
 
 Vue.component("AppHero", AppHero);
 Vue.component("AppDropdown", AppDropdown);
+Vue.component("AppSpinner", AppSpinner);
 
 Vue.filter("capitalize", value => {
   if (value && typeof value === "string") {
@@ -29,5 +32,6 @@ Vue.filter("formatDate", (value, formatType = "LL") => {
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount("#app");
---
+++ src/store/modules/threads.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export default {
+  namespaced: true,
+  state: {
+    items: []
+  },
+  getters: {},
+  actions: {
+    fetchThreads({ state, commit }, meetupId) {
+      return axios.get(`/api/v1/threads?meetupId=${meetupId}`).then(res => {
+        const threads = res.data;
+        commit(
+          "setItems",
+          { threads: "threads", items: threads },
+          { root: true }
+        );
+        return state.items;
+      });
+    }
+  },
+  mutations: {}
+};
