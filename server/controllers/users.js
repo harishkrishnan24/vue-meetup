6008d47774e121d202037cbd34d09177b9ad7f11
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-20 18:30:38 +0530

    Add server code for login, register and logout

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,13 +1,87 @@
-const User = require('../models/users');
+const User = require("../models/users");
+const passport = require("passport");
 
 exports.getUsers = function(req, res) {
-  User.find({})
-        .exec((errors, users) => {
-
+  User.find({}).exec((errors, users) => {
     if (errors) {
-      return res.status(422).send({errors});
+      return res.status(422).send({ errors });
     }
 
     return res.json(users);
   });
-}
+};
+
+exports.register = (req, res) => {
+  const registerData = req.body;
+
+  if (!registerData.email) {
+    return res.status(422).json({
+      errors: {
+        email: "is required"
+      }
+    });
+  }
+  if (!registerData.password) {
+    return res.status(422).json({
+      errors: {
+        password: "is required"
+      }
+    });
+  }
+
+  if (!registerData.password !== registerData.passwordConfirmation) {
+    return res.status(422).json({
+      errors: {
+        password: "is not the same as confirmation password"
+      }
+    });
+  }
+
+  const user = new User(registerData);
+  user.save((errors, savedUser) => {
+    if (errors) {
+      return res.status(422).json({ errors });
+    }
+    return res.json(savedUser);
+  });
+};
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(422).json({
+      errors: {
+        email: "is required"
+      }
+    });
+  }
+  if (!password) {
+    return res.status(422).json({
+      errors: {
+        password: "is required"
+      }
+    });
+  }
+
+  return passport.authenticate("local", (err, passportUser) => {
+    if (err) return next(err);
+    if (passportUser) {
+      req.login(passportUser, err => {
+        if (err) next(err);
+        return res.json(passportUser);
+      });
+    } else {
+      return res.status(422).send({
+        errors: {
+          authentication: "Ooops, Something went wrong!"
+        }
+      });
+    }
+  })(req, res, next);
+};
+
+exports.logout = (req, res) => {
+  req.logout();
+  return res.json({ status: "Session destroyed" });
+};
